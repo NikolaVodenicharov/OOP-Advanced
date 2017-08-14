5ffda8c3135c666e7fcc24da8b907766c4324101
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-08-14 21:16:46 +0300

    task 05

---
+++ InterfacesAndAbstraction/BorderControl/BorderControlExecution.cs
@@ -0,0 +1,69 @@
+﻿namespace BorderControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class BorderControlExecution
+    {
+        static void Main(string[] args)
+        {
+            var inhabitants = new List<IIdentifiable>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                bool stopLoop = input.Equals("End", StringComparison.InvariantCultureIgnoreCase);
+                if (stopLoop)
+                {
+                    break;
+                }
+
+                AddInhiabitant(inhabitants, input);
+            }
+
+            var fakeId = Console.ReadLine();
+            StringBuilder rebelionsId = ExtractRebelionsId(inhabitants, fakeId);
+
+            PrintIdsForDetaining(rebelionsId);
+        }
+
+        private static StringBuilder ExtractRebelionsId(List<IIdentifiable> inhabitants, string fakeId)
+        {
+            var rebelionsId = new StringBuilder();
+            foreach (var inhab in inhabitants)
+            {
+                var lastDigits = inhab.Id.Substring(inhab.Id.Length - fakeId.Length);
+
+                if (lastDigits == fakeId)
+                {
+                    rebelionsId.AppendLine(inhab.Id);
+                }
+            }
+
+            return rebelionsId;
+        }
+
+        private static void PrintIdsForDetaining(StringBuilder rebelionsId)
+        {
+            Console.WriteLine(rebelionsId.ToString().TrimEnd());
+        }
+
+        private static void AddInhiabitant(List<IIdentifiable> inhabitats, string input)
+        {
+            var splitInput = input.Split();
+
+            if (splitInput.Length == 2)
+            {
+                IIdentifiable inputRobot = new Robot(splitInput[0], splitInput[1]);
+                inhabitats.Add(inputRobot);
+            }
+            else if (splitInput.Length == 3)
+            {
+                var inputCitizen = new Citizen(splitInput[0], splitInput[1], splitInput[2]);
+                inhabitats.Add(inputCitizen);
+            }
+        }
+    }
+}
---
+++ InterfacesAndAbstraction/BorderControl/Citizen.cs
@@ -0,0 +1,19 @@
+﻿namespace BorderControl
+{
+    using System;
+
+    public class Citizen : IIdentifiable
+    {
+        private string name;
+        private string age;
+
+        public Citizen (string name, string age, string id)
+        {
+            this.name = name;
+            this.age = age;
+            this.Id = id;
+        }
+
+        public string Id { get; }
+    }
+}
