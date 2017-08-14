8609cafb71be81b245562603be654892b69c475a
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-08-14 19:28:33 +0300

    task 04

---
+++ InterfacesAndAbstraction/Telephony/Telephone.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class Telephone : ICall, IBrowse
+    {
+        public string Browse(string site)
+        {
+            if (isSite(site))
+            {
+                string format = $"Browsing: {site}!";
+                return format;
+            }
+
+            return "Invalid URL!";
+        }
+
+        public string Call(string number)
+        {
+            if (isPhoneNumber(number))
+            {
+                return "Calling... " + number;
+            }
+
+            return "Invalid number!";
+        }
+
+        private bool isSite(string site)
+        {
+            for (int i = 0; i < site.Length; i++)
+            {
+                if (char.IsDigit(site[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool isPhoneNumber (string number)
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (!char.IsDigit(number[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        
+    }
+}
---
+++ InterfacesAndAbstraction/Telephony/TelephonyExecution.cs
@@ -0,0 +1,24 @@
+﻿namespace Telephony
+{
+    using System;
+
+    public class TelephonyExecution
+    {
+        public static void Main(string[] args)
+        {
+            var myPhone = new Telephone();
+
+            var numbersToCall = Console.ReadLine().Split();
+            foreach (var phoneNumber in numbersToCall)
+            {
+                Console.WriteLine(myPhone.Call(phoneNumber));
+            }
+
+            var sitesToVisit = Console.ReadLine().Split();
+            foreach (var site in sitesToVisit)
+            {
+                Console.WriteLine(myPhone.Browse(site));
+            }
+        }
+    }
+}
