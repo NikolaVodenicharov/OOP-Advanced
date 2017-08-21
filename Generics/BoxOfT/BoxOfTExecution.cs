d19303fc9dcca9b0516d806bf4ca0bca746a1444
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-08-21 12:27:40 +0300

    Task 01 and 02 of Generics lab

---
+++ Generics/BoxOfT/BoxOfTExecution.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class BoxOfTExecution
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
---
+++ Generics/GenericArrayCreator/GenericArrayCreatorExecution.cs
@@ -0,0 +1,9 @@
+﻿
+public class GenericArrayCreatorExecution
+{
+    public static void Main(string[] args)
+    {
+        string[] strings = ArrayCreator.Create(5, "Pesho");
+        int[] integers = ArrayCreator.Create(10, 33);
+    }
+}
