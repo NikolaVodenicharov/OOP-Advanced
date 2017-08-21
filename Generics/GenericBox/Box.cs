2201915ef05b509836c6c7e80ef5944acb0442ce
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-08-21 16:46:47 +0300

    Task 02 exercises generics.

---
+++ Generics/GenericBox/Box.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericBox
+{
+    public class Box<T>
+    {
+        public Box(T data)
+        {
+            this.Data = data;
+        }
+
+        public T Data { get; set; }
+
+        public override string ToString()
+        {
+            var output = $"{this.Data.GetType().FullName}: {this.Data}";
+
+            return output;
+        }
+    }
+}
---
+++ Generics/GenericBox/GenericBoxExecution.cs
@@ -0,0 +1,19 @@
+﻿namespace GenericBox
+{
+    using System;
+
+    public class GenericBoxExecution
+    {
+        public static void Main()
+        {
+            var inputLinesNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputLinesNumber; i++)
+            {
+                var str = new Box<string>(Console.ReadLine());
+
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
