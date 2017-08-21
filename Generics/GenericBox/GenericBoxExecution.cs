818a6c51bebba88d61c7d5b161e5e6d75ec0c3fb
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-08-21 16:59:18 +0300

    task 03 exercises generic

--- Generics/GenericBox/GenericBoxExecution.cs
+++ Generics/GenericBox/GenericBoxExecution.cs
@@ -1,17 +1,33 @@
 ﻿namespace GenericBox
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class GenericBoxExecution
     {
         public static void Main()
         {
+            var lines = new List<Box<string>>();
+
             var inputLinesNumber = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < inputLinesNumber; i++)
             {
-                var str = new Box<int>(int.Parse(Console.ReadLine()));
+                var line = new Box<string>(Console.ReadLine());
+                lines.Add(line);
+            }
 
+            var inputIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var index1 = inputIndexes[0];
+            var index2 = inputIndexes[1];
+
+            var temporary = lines[index1];
+            lines[index1] = lines[index2];
+            lines[index2] = temporary;
+
+            foreach (var str in lines)
+            {
                 Console.WriteLine(str);
             }
         }
