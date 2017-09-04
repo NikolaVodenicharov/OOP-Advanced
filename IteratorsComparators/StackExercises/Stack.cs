b1c7cccd2c2f2ccc4e11c40ef29b250e5783f261
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-09-04 23:48:44 +0300

    Add exercises Stack<T>

---
+++ IteratorsComparators/StackExercises/Stack.cs
@@ -0,0 +1,61 @@
+﻿namespace StackExercises
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Stack<T> : IEnumerable<T>
+    {
+        private const int inititialCapacity = 10;
+        private int Count;
+
+        public Stack()
+        {
+            this.Elements = new T[inititialCapacity];
+            this.Count = 0;
+        }
+
+        public T[] Elements { get; set; }
+
+        public void Push(T element)
+        {
+            if (this.Count == this.Elements.Length)
+            {
+                Resize();
+            }
+
+            this.Elements[this.Count] = element;
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            this.Count--;
+            if (this.Count < 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+
+            T removedElement = this.Elements[Count];
+
+            return removedElement;
+        }
+
+        private void Resize()
+        {
+            T[] resizedElements = new T[this.Elements.Length * 2];
+            this.Elements.CopyTo(resizedElements, 0);
+            this.Elements = resizedElements;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0 ; i--)
+            {
+                yield return this.Elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ IteratorsComparators/StackExercises/StackExecution.cs
@@ -0,0 +1,85 @@
+﻿namespace StackExercises
+{
+    using System;
+    using System.Text;
+
+    public class StackExecution
+    {
+        public static void Main()
+        {
+            var elements = new Stack<int>();
+
+            bool isContinue = true;
+            while (isContinue)
+            {
+                var input = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                switch (input[0])
+                {
+                    case "Pop":
+                        RemoveElement(elements);
+                        break;
+                    case "Push":
+                        AddElements(elements, input);
+                        break;
+                    case "END":
+                        isContinue = false;
+                        break;
+                }
+            }
+
+            PrintElements(elements);
+        }
+
+        public static void PrintElements(Stack<int> elements)
+        {
+            var output = new StringBuilder();
+
+            foreach (var element in elements)
+            {
+                output.AppendLine(element.ToString());
+            }
+
+            output.Append(output);
+
+            Console.WriteLine(output.ToString().TrimEnd());
+        }
+
+        public static void RemoveElement(Stack<int> elements)
+        {
+            try
+            {
+                elements.Pop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void AddElements(Stack<int> elements, string[] input)
+        {
+            var elementsForAdding = ExtractElements(input);
+
+            foreach (var item in elementsForAdding)
+            {
+                elements.Push(item);
+            }
+        }
+
+        public static int[] ExtractElements(string[] input)
+        {
+            var len = input.Length - 1;
+            int[] collection = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                var parsingElement = input[i + 1];
+                var parsedElement = int.Parse(parsingElement);
+                collection[i] = parsedElement;
+            }
+
+            return collection;
+        }
+    }
+}
