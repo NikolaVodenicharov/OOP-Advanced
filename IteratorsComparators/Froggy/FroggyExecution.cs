fdf7de6c5105870c901131b49cec55ee9ba11824
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-09-09 19:58:22 +0300

    Exercises 04 Froggy.

---
+++ IteratorsComparators/Froggy/FroggyExecution.cs
@@ -0,0 +1,19 @@
+﻿namespace Froggy
+{
+    using System;
+    using System.Linq;
+
+    public class FroggyExecution
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            var lake = new Lake(input);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
---
+++ IteratorsComparators/Froggy/Lake.cs
@@ -0,0 +1,39 @@
+﻿namespace Froggy
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Lake : IEnumerable<int>
+    {
+        private readonly List<int> elements;
+
+        public Lake (IEnumerable<int> elements)
+        {
+            this.elements = new List<int>(elements);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            var count = this.elements.Count;
+
+            for (int i = 0; i < count; i += 2)
+            {
+                yield return this.elements[i];
+            }
+
+            var lastOddIndex = count - 1;
+            if (count % 2 == 1)
+            {
+                lastOddIndex--;
+            }
+
+            for (int i = lastOddIndex; i > 0; i -=2)
+            {
+                yield return this.elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
