2cbd086c58e119c2d52eb9cc7e00b76752a8285b
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-09-10 17:34:26 +0300

    Exercises 05.ComparingObjects update

---
+++ IteratorsComparators/ComparingObjects/ComparingObjectsExecution.cs
@@ -0,0 +1,81 @@
+﻿namespace ComparingObjects
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ComparingObjectsExecution
+    {
+        public static void Main()
+        {
+            var persons = new List<Person>();
+
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+
+                if (inputLine == "END")
+                {
+                    break;
+                }
+
+                persons.Add(
+                    CreatePerson(inputLine));
+            }
+
+            var collectionIndex = int.Parse(Console.ReadLine());
+            string output = CreateOutput(
+                persons, 
+                collectionIndex);
+            Console.WriteLine(output);
+        }
+
+        private static string CreateOutput(List<Person> persons, int collectionIndex)
+        {
+            var output = string.Empty;
+
+            if (collectionIndex < persons.Count)
+            {
+                var totalPeople = persons.Count;
+                int equalPeople = FindEqualPeople(
+                    persons, 
+                    collectionIndex);
+                var notEqualPeople = totalPeople - equalPeople;
+
+                output = $"{equalPeople} {notEqualPeople} {totalPeople}";
+            }
+            else
+            {
+                output = "No matches";
+            }
+
+            return output;
+        }
+
+        private static int FindEqualPeople(List<Person> persons, int collectionIndex)
+        {
+            var equalPeople = 0;
+            var targetPerson = persons[collectionIndex];
+            foreach (var person in persons)
+            {
+                if (person.CompareTo(targetPerson) == 0)
+                {
+                    equalPeople++;
+                }
+            }
+
+            return equalPeople;
+        }
+
+        private static Person CreatePerson(string inputLine)
+        {
+            var splitedInput = inputLine.Split();
+
+            var name = splitedInput[0];
+            var age = int.Parse(splitedInput[1]);
+            var town = splitedInput[2];
+
+            var person = new Person(name, age, town);
+            return person;
+        }
+    }
+}
---
+++ IteratorsComparators/ComparingObjects/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace ComparingObjects
+{
+    using System;
+
+    public class Person : IComparable<Person>
+    {
+        public Person (string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = 0;
+
+            if(this.Name != other.Name)
+            {
+                result = this.Name.CompareTo(other.Name);
+            }
+            else if (this.Age != other.Age)
+            {
+                result = this.Age - other.Age;
+            }
+            else
+            {
+                result = this.Town.CompareTo(other.Town);
+            }
+
+            return result;
+        }
+    }
+}
