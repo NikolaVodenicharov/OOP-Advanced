533817d3eaa0f05903108084a2fb8c8e2280acb9
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-09-13 15:11:55 +0300

    Add Lab 01.WeekDays

---
+++ EnumerationsAndAttributes/Weekdays/WeeklyCalendar.cs
@@ -0,0 +1,19 @@
+﻿
+    using System.Collections.Generic;
+
+    public class WeeklyCalendar
+    {
+        public WeeklyCalendar()
+        {
+            this.WeeklySchedule = new List<WeeklyEntry>();
+        }
+
+        public List<WeeklyEntry> WeeklySchedule { get;}
+
+        public void AddEntry(string weekday, string notes)
+        {
+            var weklyEntry = new WeeklyEntry(weekday, notes);
+            this.WeeklySchedule.Add(weklyEntry);
+        }
+    }
+
---
+++ EnumerationsAndAttributes/Weekdays/WeeklyEntry.cs
@@ -0,0 +1,47 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+
+    public class WeeklyEntry : IComparable<WeeklyEntry>
+    {
+        public WeeklyEntry(string weekday, string notes)
+        {
+            this.Weekday = (WeekDay)Enum.Parse(typeof(WeekDay), weekday);
+            this.Notes = notes;
+        }
+
+        public WeekDay Weekday { get; private set; }
+        public string Notes { get; private set; }
+
+        public override string ToString()
+        {
+            var output = $"{this.Weekday} - {this.Notes}";
+            return output;
+        }
+
+        public int CompareTo(WeeklyEntry other)
+        {
+            var result = 0;
+
+            if (ReferenceEquals(this, other))
+            {
+                result = 0;
+            }
+            else if (ReferenceEquals(null, other))
+            {
+                result = 1;
+            }
+            else
+            {
+                result = Weekday.CompareTo(other.Weekday);
+            }
+
+            if (result == 0)
+            {
+                result = string.Compare(this.Notes, other.Notes, StringComparison.Ordinal);
+            }
+
+            return result;
+        }
+    }
+
