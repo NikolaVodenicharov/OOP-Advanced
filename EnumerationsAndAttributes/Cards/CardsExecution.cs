d6869f7787f2d8780851bdcc2995418d223b3331
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-09-16 22:15:30 +0300

    EnumerationsAndAttributes Exercises - Problem 2.Card Rank

--- EnumerationsAndAttributes/Cards/CardsExecution.cs
+++ EnumerationsAndAttributes/Cards/CardsExecution.cs
@@ -7,7 +7,7 @@
     {
         public static void Main()
         {
-            PrintCardSuit();
+            PrintCardRank();
         }
 
         public static void PrintCardSuit()
@@ -21,6 +21,23 @@
             foreach (var card in Enum.GetValues(typeof(CardSuit)))
             {
                 output.Append($"Ordinal value: {(int)card}; Name value: {card}")
+                    .AppendLine();
+            }
+
+            Console.WriteLine(output.ToString().TrimEnd());
+        }
+
+        public static void PrintCardRank()
+        {
+            var input = Console.ReadLine();
+
+            var output = new StringBuilder();
+            output.Append(input + ":")
+                .AppendLine();
+
+            foreach (var card in Enum.GetValues(typeof(CardRank)))
+            {
+                output.AppendFormat($"Ordinal value: {(int)card}; Name value: {card}")
                     .AppendLine();
             }
 
