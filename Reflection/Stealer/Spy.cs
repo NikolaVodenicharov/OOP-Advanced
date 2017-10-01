42472aa6aaa966c9fc66632eef17255fc70a768e
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-10-01 22:07:01 +0300

    Reflection Lab 1.Stealer

---
+++ Reflection/Stealer/Spy.cs
@@ -0,0 +1,32 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Reflection;
+    using System.Text;
+    using System.Linq;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            var classType = Type.GetType(investigatedClass);
+            var classFields = classType.GetFields(
+                BindingFlags.Instance | 
+                BindingFlags.Public | 
+                BindingFlags.NonPublic | 
+                BindingFlags.Static);
+
+            var output = new StringBuilder();
+            output.AppendLine($"Class under investigation: {investigatedClass}");
+
+            var classInstance = Activator.CreateInstance(classType);
+
+            foreach (var field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                output.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Reflection/Stealer/StealerExecution.cs
@@ -0,0 +1,14 @@
+﻿namespace Stealer
+{
+    using System;
+
+    public class StealerExecution
+    {
+        public static void Main()
+        {
+            var spy = new Spy();
+            var result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
