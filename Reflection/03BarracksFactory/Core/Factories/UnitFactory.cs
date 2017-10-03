e0fea78f30c904140cbbfbd4dfe762daac2de950
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-10-03 19:49:46 +0300

    Reflection exercises, Problem 3.	BarracksWars - A New Factory.

---
+++ Reflection/03BarracksFactory/Core/Factories/UnitFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace _03BarracksFactory.Core.Factories
+{
+    using System;
+    using Contracts;
+
+    public class UnitFactory : IUnitFactory
+    {
+        private const string UnitNameSpace = "_03BarracksFactory.Models.Units.";
+
+        public IUnit CreateUnit(string unitType)
+        {
+            var type = Type.GetType(UnitNameSpace + unitType);
+            var unit = (IUnit)Activator.CreateInstance(type);
+            return unit;
+        }
+    }
+}
