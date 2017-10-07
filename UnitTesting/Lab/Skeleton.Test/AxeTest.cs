eeda968a598d09e9407c90af9c29e9954200fd10
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-10-07 21:13:31 +0300

    UnitTesting

---
+++ UnitTesting/Lab/Skeleton.Test/AxeTest.cs
@@ -0,0 +1,51 @@
+﻿
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class AxeTest
+    {
+        [Test]
+        public void AxeLosesDurabilityAfterAtack()
+        {
+            //Arange
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(100, 18);
+
+            //Act
+            axe.Attack(dummy);
+
+            //Assert
+            Assert.AreEqual(9, axe.DurabilityPoints);
+        }
+
+        [Test]
+        public void AtackWithBrokenAxeThrowException()
+        {
+            //Arange
+            Axe axe = new Axe(1, 1);
+            Dummy dummy = new Dummy(20, 8);
+
+            //Act
+            axe.Attack(dummy);
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+        }
+
+        [Test]
+        public void AtackWithBrokenAxeExceptionMessage()
+        {
+            //Arange
+            Axe axe = new Axe(1, 1);
+            Dummy dummy = new Dummy(20, 8);
+
+            //Act
+            axe.Attack(dummy);
+
+            //Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.AreEqual("Axe is broken.", ex.Message);
+        }
+    }
+
