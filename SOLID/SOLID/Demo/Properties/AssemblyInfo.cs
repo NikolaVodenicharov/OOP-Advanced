ebe27c2b5846ea17d95f27e737eca6502d5ca327
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-10-12 00:33:57 +0300

    .

--- SOLID/SOLID/Demo/Properties/AssemblyInfo.cs
+++ SOLID/SOLID/Demo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeleteMe")]
+[assembly: AssemblyTitle("Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DeleteMe")]
+[assembly: AssemblyProduct("Demo")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8243a807-30f1-43e6-8e2c-e3e07ac1038a")]
+[assembly: Guid("720466d7-bea7-45dd-9e78-aa8c716c23ba")]
 
 // Version information for an assembly consists of the following four values:
 //
