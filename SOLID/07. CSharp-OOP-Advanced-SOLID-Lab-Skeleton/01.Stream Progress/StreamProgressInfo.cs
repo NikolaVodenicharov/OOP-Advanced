8868b4ac6a2dcc13cd9e53708526f8368a31b88d
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-10-12 00:27:14 +0300

    SOLID 1.Stream Progress Info

---
+++ SOLID/07. CSharp-OOP-Advanced-SOLID-Lab-Skeleton/01.Stream Progress/StreamProgressInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.Stream_Progress
+{
+    using _01.Stream_Progress.Interfaces;
+
+    public class StreamProgressInfo
+    {
+        private IStream stream;
+
+        // If we want to stream a music file, we can't
+        public StreamProgressInfo(IStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.stream.BytesSent * 100) / this.stream.Length;
+        }
+    }
+}
