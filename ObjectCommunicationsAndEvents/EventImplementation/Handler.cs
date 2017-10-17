376a818daa6276e8559d32bd1c701f70cedb59c7
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-10-17 19:01:21 +0300

    Object communications and Events - Problem 1.	Event Implementation

---
+++ ObjectCommunicationsAndEvents/EventImplementation/Handler.cs
@@ -0,0 +1,13 @@
+﻿namespace EventImplementation
+{
+    using System;
+
+    public class Handler
+    {
+        public void OnDispatcherNameChange(object sender, NameChangeEventArgs args)
+        {
+            var name = args.Name;
+            Console.WriteLine($"Dispatcher’s name changed to {name}");
+        }
+    }
+}
---
+++ ObjectCommunicationsAndEvents/EventImplementation/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace EventImplementation
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dispatcher = new Dispatcher();
+            var handler = new Handler();
+            dispatcher.NameChange += handler.OnDispatcherNameChange;
+
+            while (true)
+            {
+                var inputName = Console.ReadLine();
+
+                var isEndOfLoop = inputName == "End";
+                if (isEndOfLoop)
+                {
+                    break;
+                }
+
+                dispatcher.Name = inputName;
+            }
+        }
+    }
+}
