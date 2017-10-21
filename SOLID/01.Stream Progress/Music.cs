ecc27f0d9c612451307cf49154676bac661ed96e
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-10-21 10:43:30 +0300

    SOLID - some finished exercises

---
+++ SOLID/01.Stream Progress/Music.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.Stream_Progress
+{
+    using _01.Stream_Progress.Interfaces;
+
+    public class Music : IStream
+    {
+        private string artist;
+        private string album;
+       
+        public Music(string artist, string album, int length, int bytesSent)
+        {
+            this.artist = artist;
+            this.album = album;
+            this.Length = length;
+            this.BytesSent = bytesSent;
+        }
+
+        public int Length { get; set; }
+
+        public int BytesSent { get; set; }
+    }
+}
---
+++ SOLID/01.Stream Progress/StreamProgressInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.Stream_Progress
+{
+    using _01.Stream_Progress.Interfaces;
+
+    public class StreamProgressInfo
+    {
+        private IStream stream;
+
+        public StreamProgressInfo(IStream stream)
+        {
+            this.stream = stream;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.stream.BytesSent * 100) / this.stream.Length;
+        }
+    }
+}
